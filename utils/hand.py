2400c3c2319073205b7f8a7bc433c8296ad57145
Andreas Nossum <andreasnossum01@hotmail.no>
2025-05-30 16:32:50 +0200

    Worked on Hand class

--- utils/hand.py
+++ utils/hand.py
@@ -3,3 +3,30 @@
 # Functionalitites: (suggestion)
 # - draw(): draw one card from deck (or face-up pile?) --> correlate with cards in deck
 # - play(): play one card from hand --> correlate with cards in deck
+
+import numpy as np
+
+class Hand:
+
+    def __init__(self):
+        self.hand = []
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def highest_card(self):
+
+        ranks = np.array([card.get_rank() for card in self.hand])
+        ranks_max = ranks.max()
+        return self.hand[ranks == ranks_max]
+    
+    def is_empty(self) -> bool:
+        return len(self) == 0
+    
+    def __len__(self) -> int:
+        return len(self.hand)
+    
+
+
+    
+
